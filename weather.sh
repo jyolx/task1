ec686aec022d4d2569a343d0a163754d04a17914
jyolx <isac.jyolsnaj@gmail.com>
2023-05-12 12:11:44 +0530

    "renamed file weather.sh"

--- weather.sh
+++ weather.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo  "Enter the city name:"
-read   "city" #getting city name as input from user
+read   "city"  #getting city name as input from user
 echo "reading weather for : ${city}"
 #connecting to the api wttr.in to retrieve data
 curl wttr.in/$city?format=3  #displaying temperature only
